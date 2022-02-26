42131afb17a4e50ac5c4098545d96672aa8ebcb6
Architvs <99622804+Architvs@users.noreply.github.com>
2022-02-26 11:29:00 +1100

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,78 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Comic Sans MS", 14.25!)
+        Me.Button1.ForeColor = System.Drawing.Color.Red
+        Me.Button1.Location = New System.Drawing.Point(111, 219)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(172, 63)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "player 1 score "
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("Comic Sans MS", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.ForeColor = System.Drawing.Color.Blue
+        Me.Button2.Location = New System.Drawing.Point(444, 219)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(172, 63)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Player 2 score "
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(285, 149)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(169, 20)
+        Me.TextBox1.TabIndex = 2
+        Me.TextBox1.Text = " "
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(767, 517)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents TextBox1 As TextBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,60 @@
+﻿Public Class Form1
+    Dim score1 As Integer
+    Dim score2 As Integer
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+        AddHandler Button1.Click, AddressOf MyClickHandler
+        AddHandler Button2.Click, AddressOf MyClickHandler
+        TextBox1.Text = ("0 :  0")
+
+    End Sub
+
+    Private Sub MyClickHandler(sender As Object, e As EventArgs)
+
+        Select Case CType(sender, Button).Name
+            Case "Button1"
+                If score1 = 0 Then
+                    score1 = score1 + 15
+                    TextBox1.Text = score1
+                ElseIf score1 = 15 Then
+                    score1 = score1 + 15
+                    TextBox1.Text = score1
+                ElseIf score1 = 30 Then
+                    score1 = score1 + 10
+                    TextBox1.Text = score2
+                ElseIf score1 = 40 Then
+                    score1 = score1 + 10
+                    TextBox1.Text = "Player 1 wins"
+                ElseIf score1 = score1 Then
+                    score1 = score1 - score1
+                    TextBox1.Text = score1
+
+                Else
+                    MsgBox("incorrect")
+                End If
+
+
+            Case "Button2"
+                If score2 = 0 Then
+                    score2 = score2 + 15
+                    TextBox1.Text = score2
+                ElseIf score2 = 15 Then
+                    score2 = score2 + 15
+                    TextBox1.Text = score2
+                ElseIf score2 = 30 Then
+                    score2 = score2 + 10
+                    TextBox1.Text = score2
+                ElseIf score2 = 40 Then
+                    score2 = score2 + 10
+                    TextBox1.Text = "Player 2 wins"
+                ElseIf score2 = 50 Then
+                    score2 = score2 - score2
+                    TextBox1.Text = score2
+                Else
+                    MsgBox("incorrect")
+                End If
+
+
+        End Select
+    End Sub
+End Class
